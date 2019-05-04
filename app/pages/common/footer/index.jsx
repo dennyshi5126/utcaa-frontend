fa4cc185b7b159f6e2428d8ed97d18260dbfa706
HaoyangLiang-at-140766880761 <haoyang.liang@futurera.io>
2019-05-04 16:09:40 -0400

    inittial setup

---
+++ app/pages/common/footer/index.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+export default class Footer extends Component {
+  render() {
+    return <div className="footer">© 2017 University of Toronto Chinese Alumni Association</div>;
+  }
+}

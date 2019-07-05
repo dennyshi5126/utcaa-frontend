bc67d56656bd9f366c9bc5ebe835dd53ff16b1e2
Bo Liu <50053312+bobliu96@users.noreply.github.com>
2019-07-04 21:15:50 -0400

    footer (#24)
    * footer
    
    * revise color

--- app/pages/common/footer/index.jsx
+++ app/pages/common/footer/index.jsx
@@ -3,6 +3,6 @@ import './index.scss';
 
 export default class Footer extends Component {
   render() {
-    return <div className="footer">© 2017 University of Toronto Chinese Alumni Association</div>;
+    return <div className="footer">© 2019 University of Toronto Chinese Alumni Association</div>;
   }
 }

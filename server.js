00c85092464f8054f370b42c40c638753eb12e09
HaoyangLiang-at-140766880761 <haoyang.liang@futurera.io>
2019-09-16 01:37:40 -0400

    updated routing for error pages

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
-const bodyParser = require("body-parser");
-const corsFilter = require("cors");
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const corsFilter = require('cors');
 
 const app = express();
-app.set("port", 80);
-app.use(logger("dev"));
+app.set('port', 80);
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(corsFilter());
-app.options("*", corsFilter()); // allow preflight (http.OPTIONS)
+app.options('*', corsFilter()); // allow preflight (http.OPTIONS)
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const mode = process.env.MODE || "";
-
-app.listen(app.get("port"), function() {
-  console.log("Express server listening on port " + app.get("port"));
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.get("/*", function(req, res, next) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+app.get('/*', (_, res, next) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/*', (_, _, res, _) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
